4304082e7293d1be51700b8e3080aa4edba8481c
DiegoH81 <hidal8909ins@gmail.com>
2026-05-24 14:42:14 -0500

    feat(client.py, server.py): add basic client and sv implementation

---
+++ client_NN.py
@@ -0,0 +1,105 @@
+# Commented out IPython magic to ensure Python compatibility.
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch.optim as optim
+import numpy as np
+import pandas as pd
+
+X = None
+y = None
+
+class MulticlassClassifier(nn.Module):
+    def __init__(self, input_dim: int, num_classes: int, hidden1: int = 128, hidden2: int = 64):
+        super(MulticlassClassifier, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden1)
+        self.fc2 = nn.Linear(hidden1, hidden1)
+        self.fc3 = nn.Linear(hidden1, hidden1)
+        self.fc4 = nn.Linear(hidden1, hidden2)
+        self.class_logits = nn.Linear(hidden2, num_classes)      # Predict class scores
+        self.class_log_vars = nn.Linear(hidden2, num_classes)     # Predict log-variance for each class
+
+    def forward(self, x: torch.Tensor):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        logits = self.class_logits(x)
+        log_vars = self.class_log_vars(x)
+        return logits, log_vars
+        #return logits, logits
+
+
+
+# Generate synthetic heteroscedastic multiclass data
+torch.manual_seed(42)
+num_samples = 1000
+input_dim = 14
+num_classes = 3
+
+
+
+####  Get Data as string from protocol
+data = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14])
+
+# Assume first 4 columns are input features, last 3 are one-hot class labels
+X_np = data[:, : input_dim]
+y_onehot_np = data[:, -num_classes: ]
+
+
+X = torch.tensor(X_np)
+
+# argmax?
+y = torch.tensor(y_onehot_np)
+
+
+#print("y ",y)
+#print("y ",y.size())
+#print("X ",X.size())
+
+
+# ModelSetup
+model = MulticlassClassifier(input_dim = input_dim, num_classes = num_classes)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+
+
+
+# Load Model Data
+
+
+
+# Training
+while(True):
+    model.train()
+
+    optimizer.zero_grad()
+
+    ####### Get  matrix
+    ####### Load Matrix from MasterC to Master P
+
+    # new_matrix_1, new_matrix_2, new_matrix_3, new_matrix_4 = proto_get_matrix()
+    # model.fc1.weight.data.copy_(torch.from_numpy(np.asarray(new_weights_matrix_1)))
+    # model.fc2.weight.data.copy_(torch.from_numpy(np.asarray(new_weights_matrix_2)))
+    # model.fc3.weight.data.copy_(torch.from_numpy(np.asarray(new_weights_matrix_3)))
+    # model.fc4.weight.data.copy_(torch.from_numpy(np.asarray(new_weights_matrix_4)))
+
+    logits, log_vars = model(X)
+    loss = criterion(logits, y)
+    loss.backward()
+    optimizer.step()
+
+    ####### Extract matrix from NN
+    ####### Send matrix to master P
+
+    # to_send_1 = np.matrix(model.fc1.weight.data.cpu().numpy())
+    # to_send_2 = np.matrix(model.fc2.weight.data.cpu().numpy())
+    # to_send_3 = np.matrix(model.fc3.weight.data.cpu().numpy())
+    # to_send_4 = np.matrix(model.fc4.weight.data.cpu().numpy())
+
+    # proto.send_matrix(to_send_1)
+    # proto.send_matrix(to_send_2)
+    # proto.send_matrix(to_send_3)
+    # proto.send_matrix(to_send_4)
--- server_NN.py
+++ server_NN.py
@@ -12,7 +12,6 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classifica
 import numpy as np
 
 import pandas as pd
-import math
 
 import torch.distributions as dist
 
@@ -23,13 +22,17 @@ class MulticlassClassifier(nn.Module):
     def __init__(self, input_dim: int, num_classes: int, hidden1: int = 128, hidden2: int = 64):
         super(MulticlassClassifier, self).__init__()
         self.fc1 = nn.Linear(input_dim, hidden1)
-        self.fc2 = nn.Linear(hidden1, hidden2)
+        self.fc2 = nn.Linear(hidden1, hidden1)
+        self.fc3 = nn.Linear(hidden1, hidden1)
+        self.fc4 = nn.Linear(hidden1, hidden2)
         self.class_logits = nn.Linear(hidden2, num_classes)      # Predict class scores
         self.class_log_vars = nn.Linear(hidden2, num_classes)     # Predict log-variance for each class
 
     def forward(self, x: torch.Tensor):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
         logits = self.class_logits(x)
         log_vars = self.class_log_vars(x)
         return logits, log_vars
@@ -37,19 +40,13 @@ class MulticlassClassifier(nn.Module):
 
 
 
-# Generate synthetic heteroscedastic multiclass data
+
 torch.manual_seed(42)
-num_samples = 1000
+
 input_dim = 14
 num_classes = 3
 batch_size = 100
 
-X = torch.randn(num_samples, input_dim)  # 1000 samples, 100 inputs
-
-active_indices = torch.randint(0, num_classes, (num_samples,))
-y = torch.nn.functional.one_hot(active_indices, num_classes=num_classes).float()
-
-#y = torch.randint(0, num_classes, (num_samples * num_classes,)).view(num_samples, num_classes)
 
 # Load dataset from CSV
 csv_path = "Dataset of Diabetes.csv"  # replace with your actual CSV file path   1000,13,3,50
@@ -59,10 +56,9 @@ df = pd.read_csv(csv_path,header=None, skiprows=1)
 
 # Assume first 4 columns are input features, last 3 are one-hot class labels
 X_np =        df.iloc[:, :input_dim].values.astype(np.float32)
-#y_onehot_np = df.iloc[:, input_dim:].values.astype(np.float32)
 y_onehot_np = df.iloc[:, -num_classes:].values.astype(np.float32)
 
-#y_np = np.argmax(y_onehot_np, axis=1).astype(np.int64)
+
 
 
 X = torch.tensor(X_np)
@@ -70,6 +66,7 @@ y = torch.tensor(y_onehot_np)
 print("y ",y)
 print("y ",y.size())
 print("X ",X.size())
+
 # Create dataset and split into training/testing
 dataset = TensorDataset(X, y)
 
@@ -90,49 +87,68 @@ optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
 num_epochs = 360
 train_tracker, test_tracker, accuracy_tracker = [], [], []
 
+
+
+model.train()
+epoch_loss = 0
+for batch_x, batch_y in train_loader:
+    optimizer.zero_grad()
+
+
+    # load matrix froma Master C starting from second time
+    # new_matrix_1, new_matrix_2, new_matrix_3, new_matrix_4 = proto_get_matrix()
+    # model.fc1.weight.data.copy_(torch.from_numpy(np.asarray(new_weights_matrix_1)))
+    # model.fc2.weight.data.copy_(torch.from_numpy(np.asarray(new_weights_matrix_2)))
+    # model.fc3.weight.data.copy_(torch.from_numpy(np.asarray(new_weights_matrix_3)))
+    # model.fc4.weight.data.copy_(torch.from_numpy(np.asarray(new_weights_matrix_4)))
+
+    logits, log_vars = model(batch_x)
+    loss = criterion(logits, batch_y)
+    loss.backward()
+    optimizer.step()
+
+    # get the matrix and distribute it to the clients
+    # to_send_1 = np.matrix(model.fc1.weight.data.cpu().numpy())
+    # to_send_2 = np.matrix(model.fc2.weight.data.cpu().numpy())
+    # to_send_3 = np.matrix(model.fc3.weight.data.cpu().numpy())
+    # to_send_4 = np.matrix(model.fc4.weight.data.cpu().numpy())
+
+    # proto.send_matrix(to_send_1)
+    # proto.send_matrix(to_send_2)
+    # proto.send_matrix(to_send_3)
+    # proto.send_matrix(to_send_4)
+
+    epoch_loss += loss.item()
+train_tracker.append(epoch_loss / len(train_loader))
+
 # Evaluation on test set
-model.eval()
 y_true = []
 y_pred = []
 log_vars_all = []
 
-for epoch in range(num_epochs):
-    model.train()
-    epoch_loss = 0
-    for batch_x, batch_y in train_loader:
-        optimizer.zero_grad()
-        logits, log_vars = model(batch_x)
-        loss = criterion(logits, batch_y)
-        loss.backward()
-        optimizer.step()
-        epoch_loss += loss.item()
-    train_tracker.append(epoch_loss / len(train_loader))
-    print(f"Epoch {epoch+1}/{num_epochs}, Loss: {train_tracker[-1]:.4f} | ",end="")
-
-
 
 #with torch.no_grad():
-    test_loss = 0
-    total = 0
-    num_correct = 0
-    for batch_x, batch_y in test_loader:
-        logits, log_vars = model(batch_x)
-        loss = criterion(logits, batch_y)
-        test_loss += loss.item()
+test_loss = 0
+total = 0
+num_correct = 0
+for batch_x, batch_y in test_loader:
+    logits, log_vars = model(batch_x)
+    loss = criterion(logits, batch_y)
+    test_loss += loss.item()
 
-        predictions = torch.argmax(logits, dim=1)
-        total += batch_x.size(0)
-        num_correct += (predictions == torch.argmax(batch_y, dim=1)).sum().item()
+    predictions = torch.argmax(logits, dim=1)
+    total += batch_x.size(0)
+    num_correct += (predictions == torch.argmax(batch_y, dim=1)).sum().item()
 
-        predictions = torch.argmax(logits, dim=1)
-        y_true.extend(torch.argmax(batch_y, dim=1))
-        y_pred.extend(predictions.tolist())
-        log_vars_all.append(log_vars)
+    predictions = torch.argmax(logits, dim=1)
+    y_true.extend(torch.argmax(batch_y, dim=1))
+    y_pred.extend(predictions.tolist())
+    log_vars_all.append(log_vars)
 
-    test_tracker.append(test_loss/len(test_loader))
-    print(f"Test loss: {test_loss/len(test_loader)} | ", end='')
-    accuracy_tracker.append(num_correct/total)
-    print(f'Accuracy : {num_correct/total}')
+test_tracker.append(test_loss/len(test_loader))
+print(f"Test loss: {test_loss/len(test_loader)} | ", end='')
+accuracy_tracker.append(num_correct/total)
+print(f'Accuracy : {num_correct/total}')
 
 
 
